51a13d22bb81c3fa3859eb7dc02c830384631468
jorrit <35707261+jorritvanderheide@users.noreply.github.com>
2024-07-10 17:52:24 +0200

    docs: annotate flake

--- flake.nix
+++ flake.nix
@@ -9,6 +9,7 @@
   description = "My Nix Config";
 
   inputs = {
+    # External dependencies this flake depends on
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default-linux";
 
@@ -32,6 +33,7 @@
   };
 
   outputs = {
+    # Defines the outputs of the flake
     self,
     nixpkgs,
     systems,
@@ -39,31 +41,37 @@
     nixos-hardware,
     ...
   } @ inputs: let
-    inherit (self) outputs;
-    lib = nixpkgs.lib // home-manager.lib;
+    inherit (self) outputs; # Inherit outputs for easy reference
+    lib = nixpkgs.lib // home-manager.lib; # Merge libraries from nixpkgs and home-manager
+
+    # Function to apply configurations across supported systems
     forEachSystem = configurePackages: lib.genAttrs (import systems) (system: configurePackages pkgsFor.${system});
+
+    # Defines packages for each system
     pkgsFor = lib.genAttrs (import systems) (
       system:
         import nixpkgs {
           inherit system;
-          config.allowUnfree = true;
+          config.allowUnfree = true; # Allow unfree packages
         }
     );
   in {
-    inherit lib;
+    inherit lib; # Make lib globally available
 
+    # Nix daemon settings, enabling optimizations and experimental features
     nix.settings = {
       auto-optimise-store = true;
       experimental-features = ["flakes" "nix-command"];
     };
 
+    # NixOS configurations, specifying system-specific modules
     nixosConfigurations = {
       # Lappie
       lappie = lib.nixosSystem {
         modules = [
           ./hosts/lappie
         ];
-        specialArgs = {inherit inputs;};
+        specialArgs = {inherit inputs;}; # Pass inputs as special arguments
       };
     };
   };
