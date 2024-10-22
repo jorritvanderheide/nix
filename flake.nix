d3878a025d448975fb0d184d6bda6ace91714718
jorrit <35707261+jorritvanderheide@users.noreply.github.com>
2024-10-22 14:47:05 +0200

    v5

--- flake.nix
+++ flake.nix
@@ -4,12 +4,14 @@
   inputs = {
     # NixOS inputs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixos-hardware.url = "github:nixos/nixos-hardware";
 
     # Community inputs
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    impermanence.url = "github:nix-community/impermanence";
   };
 
   outputs = {
--- home.nix
+++ home.nix
@@ -4,14 +4,68 @@
   pkgs,
   ...
 }: {
-  home.username = "jorrit";
-  home.homeDirectory = "/home/jorrit";
+  imports = [
+    inputs.impermanence.nixosModules.home-manager.impermanence
+  ];
+
+  home = {
+    username = "jorrit";
+    homeDirectory = "/home/jorrit";
+
+    persistence."/persist/home/jorrit" = {
+      allowOther = true;
+      directories = [
+        # User folders
+        "Desktop"
+        "Documents"
+        "Downloads"
+        "Git"
+        "Music"
+        "Pictures"
+        "Public"
+        "Templates"
+        "Videos"
+
+        # Configuration folders
+        ".config/BraveSoftware/Brave-Browser"
+        ".config/Code"
+        ".config/discord"
+        ".config/git"
+        ".config/obsidian"
+        ".config/Pinta"
+        ".config/Postman"
+        ".config/Signal"
+        ".config/spotify"
+        ".config/teams-for-linux"
+        ".vscode"
+
+        # Hidden folders
+        ".ssh"
+      ];
+      files = [
+        ".config/monitors.xml"
+        ".screenrc"
+      ];
+    };
+
+    stateVersion = "24.05";
+  };
+
+  # Programs configuration
+
+  ## Git
+  programs.git = {
+    enable = true;
+    userName = "jorrit";
+    userEmail = "35707261+jorritvanderheide@users.noreply.github.com";
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
+  };
 
   # Enable home-manager
   programs.home-manager.enable = true;
 
   # Reload system units when changing configs
   systemd.user.startServices = "sd-switch";
-
-  home.stateVersion = "24.05";
 }
