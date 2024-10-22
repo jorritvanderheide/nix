529a45ed5251b2a78893ca0bddc171625a9ed89e
jorrit <35707261+jorritvanderheide@users.noreply.github.com>
2024-10-22 12:46:07 +0200

    v3

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
 
     # Community inputs
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
