e191be43286541aa39b7de987a76a5403048455a
jorrit <35707261+jorritvanderheide@users.noreply.github.com>
2024-11-01 07:32:03 +0100

    build: generation 533

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,10 +1,10 @@
 let
-  framework = "ssh-rsa ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOTl85Z4yCznWRzkKwT+SmTPRByZyL7P+h3/hapaqP9N nixos@framework";
-  hosts = [framework];
+  framework = "ssh-rsa ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOTl85Z4yCznWRzkKwT+SmTPRByZyL7P+h3/hapaqP9N root@framework";
+  # hosts = [framework];
 
-  jorrit = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDRKO869CD/GX/yWG/dJuQSdENRPFlcbt65BsX+4wmknppoSJW3HGc+aj23JJ6IDZ4KmMSJsBvHKw8NS+nPuQ4sfnfzLY4l5DhCQlQW7ASZ4tJKejyAghxh9pcbQ6FTydzdwXy5E469SgYiy+YQ3sTr/sP2ih84dZdsxjHMsuQwnN26iShJINagvaa9hdNQdubVomAvyI4HErMyN0XH0fLj9UVm3GDoDSa0XQBWk4cLtbSpDjx0PqWEfqPv1haAGd4SOXVi4118D3DSGHniTD2U62VG94xZe47pl1IDTNQCtFJYP7UEDzXEk00xYbrm1CsxLDr/gxY8W+xoYWH/WRdKukguDpJ3hbSQtejDNJUUDqFU4yWhNPyJu/qv6+dFm0PBi0C5BrperbEhZNjYUlHYKtgDSvoIs1EV2ajKR54si8ptzZWtzC5k6E6PZXw1Hy5LO7jR0Te5BP4tWIr5VNWaTQ5AHHt99v+BlNyC4y6bLSmDgET8VsNZMqMt2e3rZJM= jorrit";
-  users = [jorrit];
+  jorrit = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDRKO869CD/GX/yWG/dJuQSdENRPFlcbt65BsX+4wmknppoSJW3HGc+aj23JJ6IDZ4KmMSJsBvHKw8NS+nPuQ4sfnfzLY4l5DhCQlQW7ASZ4tJKejyAghxh9pcbQ6FTydzdwXy5E469SgYiy+YQ3sTr/sP2ih84dZdsxjHMsuQwnN26iShJINagvaa9hdNQdubVomAvyI4HErMyN0XH0fLj9UVm3GDoDSa0XQBWk4cLtbSpDjx0PqWEfqPv1haAGd4SOXVi4118D3DSGHniTD2U62VG94xZe47pl1IDTNQCtFJYP7UEDzXEk00xYbrm1CsxLDr/gxY8W+xoYWH/WRdKukguDpJ3hbSQtejDNJUUDqFU4yWhNPyJu/qv6+dFm0PBi0C5BrperbEhZNjYUlHYKtgDSvoIs1EV2ajKR54si8ptzZWtzC5k6E6PZXw1Hy5LO7jR0Te5BP4tWIr5VNWaTQ5AHHt99v+BlNyC4y6bLSmDgET8VsNZMqMt2e3rZJM= jorrit@framework";
+  # users = [jorrit];
 in {
   # Jorrit
-  "jorrit.age".publicKeys = [jorrit] ++ hosts;
+  "jorrit.age".publicKeys = [jorrit framework];
 }
