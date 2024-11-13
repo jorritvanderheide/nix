0b5ffddea03c1e3747100e8b3be8eeb16221c7be
jorrit <35707261+jorritvanderheide@users.noreply.github.com>
2024-11-13 10:03:04 +0100

    feat: exclude packages from gnome

--- modules/nixos/features/gnome/default.nix
+++ modules/nixos/features/gnome/default.nix
@@ -1,5 +1,26 @@
-{...}: {
+{pkgs, ...}: {
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+
+  # Eclude packages from Gnome
+  environment.gnome.excludePackages = with pkgs; [
+    baobab
+    geary
+    gnome-calendar
+    gnome-characters
+    gnome-clocks
+    gnome-connections
+    gnome-console
+    gnome-contacts
+    gnome-maps
+    gnome-music
+    gnome-tour
+    gnome-weather
+    gnome-text-editor
+    simple-scan
+    totem
+    xterm
+    yelp
+  ];
 }
